089629405e471e20a36fb7d3384790e47e4bb43d
komuro8 <jheisson@gmail.com>
2025-08-14 21:06:43 -0500

    modificacion codigo GO

---
+++ backend/go.mod
@@ -0,0 +1,7 @@
+module dockergo
+
+go 1.22
+
+require (
+    github.com/lib/pq v1.10.9
+)
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-    fmt.Println("Hola Mundo 2")
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPass := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPass, dbName,
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Error conectando a la base de datos: %v", err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("No se pudo hacer ping a la DB: %v", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Conexión a Postgres exitosa!")
+	})
+
+	log.Println("Servidor escuchando en :8080")
+	http.ListenAndServe(":8080", nil)
 }
